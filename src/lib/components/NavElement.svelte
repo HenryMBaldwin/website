188fd53175a9c0d9f1bfcc4fbfc49e9a15e1484d
Henry Baldwin <henrymbaldwin@proton.me>
2026-09-04 13:33:58 -0400

    resolve nav

--- src/lib/components/NavElement.svelte
+++ src/lib/components/NavElement.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { resolve } from '$app/paths';
 	let { pathname, name } = $props();
 
 	let active = $derived(page.url.pathname.startsWith(pathname));
 </script>
 
 <div class="{active ? 'bg-black text-white' : ''} px-1">
-	<a href={pathname} aria-current={active}>{name}</a>
+	<a href={resolve(pathname)} aria-current={active}>{name}</a>
 </div>
